fbf084e1b7eebc259d18d1bf61468d6b5857887a
koushik <kowshik80903@gmail.com>
2019-07-03 21:49:29 +0530

    selection sort added

---
+++ selection_sort.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+void swap(int * a,int * b){
+    int temp=*a;
+    *a=*b;
+    *b=temp;
+}
+void selection_sort(int arr[],int n){
+    for(int i=0;i<n;i++)
+    {
+        int min=i;
+        for(int j=i+1;j<n;j++)
+        {
+            if(arr[j]<arr[i]){
+                min=j;
+                swap(&arr[min],&arr[i]);
+            }
+        }
+    }
+}
+int main(void)
+{
+   ll n;
+   cin>>n;
+   int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    selection_sort(arr,n);
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
